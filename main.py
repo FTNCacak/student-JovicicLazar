fe639ca9b22fdd5aa7ec11257d439f4f633f5dc2
JovicicLazar <lazarjovicic808@gmail.com>
2021-10-19 14:37:34 -0500

    First commit

---
+++ main.py
@@ -0,0 +1,19 @@
+import re
+
+def makeString(text):
+    text = text.lower()
+    regex = re.compile('[^a-zA-Z]')
+    text = regex.sub('', text)
+    return text.strip()
+
+def is_palindrome(text, i = 0):
+    text = makeString(text)
+    if text[i] != text[(len(text) - 1) - i]:
+        return False
+
+    if i == (len(text)//2):
+        return True
+
+    return is_palindrome(text, i + 1)
+
+print(is_palindrome("Taco cat"))
